9e4714b69dcb393a7e82d6ed1fa08d33a56219e5
kimdat0705 <kimdat0705@gmil.com>
2020-10-19 10:52:30 +0700

    Add ex27

---
+++ ex27/main.py
@@ -0,0 +1,34 @@
+
+NOT True?
+notFalse True
+notTrue False
+OR True?
+TrueorFalse True
+TrueorTrue True
+FalseorTrue True
+FalseorFalse False
+AND True?
+TrueandFalse False
+TrueandTrue True
+FalseandTrue False
+FalseandFalse False
+NOT OR True?
+Not (TrueorFalse) False
+Not (TrueorTrue) False
+Not (FalseorTrue) False
+Not (FalseorFalse) True
+NOT AND True?
+Not (TrueandFalse) True
+Not (TrueandTrue) False
+Not (FalseandTrue) True
+Not (FalseandFalse) True
+!= True?
+1!=0 True
+1!=1 False
+0!=1 True
+0!=0 False
+== True?
+1==0 False
+1==1 True
+0==1 False
+0==0 True
